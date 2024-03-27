44066f888921970af474c069bc26f20dc45d6da6
Bektemir2001 <bektemirkumarbayuulu@gmail.com>
2024-03-27 13:51:57 +0600

    save changes

---
+++ resources/views/includes/site_bar.blade.php
@@ -0,0 +1,33 @@
+<div class="iq-sidebar  sidebar-default  ">
+    <div class="iq-sidebar-logo d-flex align-items-end justify-content-between">
+
+        <div class="side-menu-bt-sidebar-1">
+            <svg xmlns="http://www.w3.org/2000/svg" class="text-light wrapper-menu" width="30" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+            </svg>
+        </div>
+    </div>
+    <div class="data-scrollbar" data-scroll="1">
+        <nav class="iq-sidebar-menu">
+            <ul id="iq-sidebar-toggle" class="side-menu">
+                <li class="px-3 pt-3 pb-2">
+                    <span class="text-uppercase small font-weight-bold">Pages</span>
+                </li>
+
+                <li class=" sidebar-layout">
+                    <a href="" class="svg-icon">
+                        <i class="">
+
+                            <svg xmlns="http://www.w3.org/2000/svg" width="18" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M9.568 3H5.25A2.25 2.25 0 0 0 3 5.25v4.318c0 .597.237 1.17.659 1.591l9.581 9.581c.699.699 1.78.872 2.607.33a18.095 18.095 0 0 0 5.223-5.223c.542-.827.369-1.908-.33-2.607L11.16 3.66A2.25 2.25 0 0 0 9.568 3Z" />
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M6 6h.008v.008H6V6Z" />
+                            </svg>
+
+                        </i><span class="ml-2">Categories</span>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+        <div class="pt-5 pb-5"></div>
+    </div>
+</div>
